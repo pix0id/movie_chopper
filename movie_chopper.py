a15ed3fbe023b965dd4a312399249a4bfcc42487
Alex Sollman <alex@alexsollman.dev>
2022-10-14 23:53:54 -0700

    Refactor work. Updating clip logic based on feedback.

--- movie_chopper.py
+++ movie_chopper.py
@@ -1,6 +1,7 @@
 from moviepy.editor import *
 import os
 import argparse
+import random
 
 # Directories and files
 parser = argparse.ArgumentParser()
@@ -11,44 +12,102 @@ parser.add_argument("-c", "--clips", dest="clips_folder", default="movies/clips/
 
 args = parser.args()
 
-video_path = "movies/"
-clip_path = f"movies/clips/"
-movies = []
+class MovieChop:
+    def __init__(self, length, number_of_clips, video_path, clip_path,):
+        self.length = int(length)
+        self.number_of_clips = int(number_of_clips)
+        self.video_path = video_path
+        self.clip_path = clip_path
+        self.length_in_seconds = int(60*length)
+        self.movies = []
+        self.touched_movies = []
+        self.count = 0
 
-# time/name logic variables
-clip_length_minutes = args.length
-clip_length_seconds = 60 * clip_length_minutes
-count = 1
-# TODO: Clip videos at random timestamps instead of splitting up like it is now.
-# TODO: Choose which video to clip randomly.
-# TODO: Do not clip from same video until all other videos have been clipped from.
-# TODO: Only create the number of clips specified in args.number_of_clips.
-# TODO: After clipping is complete, merge all videos together into one file.
-# TODO: Remove audio and replace it with ~*~V I B E S~*~
+    def get_movies(self):
+        '''
+            Generates list of videos from specified video path.
+            Does not work with nested directories.
+            Make sure the videos are the ONLY thing in the directory. ALL FILES are added to the list currently.
+            TODO:
+                - Make work with nested directories
+                - specify accepted video types
+        '''
+        for file in os.listdir(self.video_path):
+            if os.path.isfile(os.path.join(self.video_path, file)):
+                self.movies.append(video)
 
-# get all files listed in /movies/
-for video in os.listdir(video_path):
-    if os.path.isfile(os.path.join(video_path, video)):
-        movies.append(video)
+    def set_clip_buffer(self, video_length, clip_length):
+        '''
+            Creates buffer space at end of video, so you cannot get a clip shorter than the specified amount.
+        '''
+        return video_length - clip_length
+
+    def clip_random_movie(self):
+        '''
+            Chooses a random file from the list of movies
+            Checks if the movie has been "touched" recently 
+            (Meaning other movies haven't been "touched" recently.)
+            Clips chosen movie and writes file
+        '''
+        current_movie_name = random.choice(self.movies)
+        movie_length = current_movie.duration
+        clip_buffer = self.set_clip_buffer(movie_length, self.length_in_seconds)
+
+        if current_movie not in self.touched_movies:
+            self.count += 1
+            current_movie = VideoFileClip(f"{self.video_path}{current_movie_name}")
+            start_index = random.randint(0, clip_buffer)
+
+            clip = video.subclip(start_index, self.length_in_seconds)
+            clip.write_videofile(f"{self.clip_path}{self.count + 1}.mp4",codec="libx264")
+
+            clip.close()
+        else:
+            """Sort lists and check if they're the same. If so, empty touched_movies and start chopping again"""
+            self.movies.sort()
+            self.touched_movies.sort()
+
+            if self.movies == self.touched_movies:
+                self.touched_movies = []
+
+
+
+# video_path = "movies/"
+# clip_path = f"movies/clips/"
+# movies = []
+
+# # time/name logic variables
+# clip_length_minutes = args.length
+# clip_length_seconds = 60 * clip_length_minutes
+# count = 1
+# # TODO: Refactor.
+
+# # TODO: Only create the number of clips specified in args.number_of_clips.
+# # TODO: After clipping is complete, merge all videos together into one file.
+# # TODO: Remove audio and replace it with ~*~V I B E S~*~
+
+# # # get all files listed in /movies/
+# # for video in os.listdir(video_path):
+# #     if os.path.isfile(os.path.join(video_path, video)):
+# #         movies.append(video)
         
-# Loop through all the movies in the movies folder
-for index, movie in enumerate(movies):
-    video = VideoFileClip(f"{video_path}{movie}")
-    time_tracker = 0
-    movie_length = video.duration
+# # Loop through all the movies in the movies folder
+# for index, movie in enumerate(movies):
+#     video = VideoFileClip(f"{video_path}{movie}")
+#     time_tracker = 0
+#     movie_length = video.duration
 
-    print(f"Now chopping {movie} ({index + 1}/{len(movies)})")
+#     print(f"Now chopping {movie} ({index + 1}/{len(movies)})")
 
-    while time_tracker < movie_length:
-        remaining_time = movie_length - time_tracker
-        # If statement here to prevent errors if clip will be shorter than the remaining time in the movie.
-        # Won't matter really since 99% of the time this will prevent one clip that is 100% credits.
-        # Marvel post-credit scene fans in shambles.
-        if remaining_time > clip_length_seconds:
-            clip = video.subclip(time_tracker, clip_length_seconds*count)
-            clip.write_videofile(f"{clip_path}{count}.mp4",codec="libx264")
+#     while time_tracker < movie_length:
+#         remaining_time = movie_length - time_tracker
+#         # If statement here to prevent errors if clip will be shorter than the remaining time in the movie.
+#         # Won't matter really since 99% of the time this will prevent one clip that is 100% credits.
+#         # Marvel post-credit scene fans in shambles.
+#         if remaining_time > clip_length_seconds:
+#             clip = video.subclip(time_tracker, clip_length_seconds*count)
+#             clip.write_videofile(f"{clip_path}{count}.mp4",codec="libx264")
+#             clip.close()
 
-        count += 1
-        time_tracker += clip_length_seconds
-
-    clip.close()
+#         count += 1
+#         time_tracker += clip_length_seconds
