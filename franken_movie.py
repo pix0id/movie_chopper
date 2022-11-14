723bd98f397c4ab66c450afe2a46a656fad45d78
Alex Sollman <alex@alexsollman.dev>
2022-11-13 17:43:06 -0800

    updated concat_clips in franken_movie to match settings updates

--- franken_movie.py
+++ franken_movie.py
@@ -6,16 +6,16 @@ import movie_chopper as mc
 # TODO: Count up concat file name so multiple can exist in directory
 # TODO: Clip folder cleanup
 
-def concat_clips(clips_folder):
-    clips = mc.get_video_files(movie_path=CLIP_PATH)
+def concat_clips(video_folder):
+    clips = mc.get_video_files(video_folder)
     clip_list = []
 
     for clip in clips:
-        clip_file = VideoFileClip(f"{CLIP_PATH}{clip}")
+        clip_file = VideoFileClip(f"{video_folder}{clip}")
         clip_list.append(clip_file)
     
     final_clip = concatenate_videoclips(clip_list, method="compose")
     final_clip.write_videofile("concat.mp4")
 
 # F R A N K E N M O V I E
-concat_clips(clips_folder=CLIP_PATH)
+concat_clips(video_folder=CLIP_PATH)
