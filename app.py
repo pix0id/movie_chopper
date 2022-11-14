8d436e9d80a729a95512d40ceaf75ff3820c2b8e
Alex Sollman <alex@alexsollman.dev>
2022-11-13 17:45:38 -0800

    updated app.py settings constants

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import os
 import movie_chopper as mc
 import franken_movie as fm
-import settings
+from settings import *
 # do we need this?
 count = 0
 
 # get movies list
-movies = mc.get_video_files(video_path=settings.video_path)
+movies = mc.get_video_files(video_path=VIDEO_PATH)
 
 
 # Create clips
-while count < settings.number_of_clips:
+while count < NUMBER_OF_CLIPS:
     mc.clip_random_movie(movies)
 
 
 # F R A N K E N M O V I E
-fm.concat_clips(clips_folder=settings.clip_path)
+fm.concat_clips(video_folder=CLIP_PATH)
